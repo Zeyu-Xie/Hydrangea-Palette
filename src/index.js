21c25ff5d1d15f9561c17435efaba1c329c70528
Acan <acanthuridae@foxmail.com>
2023-07-15 14:04:20 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import ReacrDOM from "react-dom"
+import ReactDOM from "react-dom"
 import store from "./store/store"
 import { Provider } from "react-redux"
 import App from "./App"
 
-ReacrDOM.render(
+ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, document.getElementById("root")
